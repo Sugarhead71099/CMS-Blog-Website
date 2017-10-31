7b4e006947cb93281c0993603c48f27ed70fd4e3
Braijon Glover <26751990+Sugarhead71099@users.noreply.github.com>
2017-10-30 21:21:27 -0500

    update createTables.php
    Added more table create queries

--- includes/createTables.php
+++ includes/createTables.php
@@ -3,15 +3,15 @@
 	// Categories & Categories Meta Tables
 	$categoriesTable = "CREATE TABLE IF NOT EXISTS categories (
 							category_id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT,
-							category_title VARCHAR(255) NOT NULL,
+							category_title VARCHAR(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
 							PRIMARY KEY (category_id)
 						) ENGINE = InnoDB;";
 
 	$categoriesMetaTable = "CREATE TABLE IF NOT EXISTS categories_meta (
 							meta_id INT(8) UNSIGNED NOT NULL AUTO_INCREMENT,
 							category_id INT(4) UNSIGNED NOT NULL,
-							meta_key VARCHAR(255) NOT NULL,
-							meta_value VARCHAR(255) NOT NULL,
+							meta_key VARCHAR(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+							meta_value VARCHAR(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
 							PRIMARY KEY (meta_id)
 						) ENGINE = InnoDB;";
 
@@ -25,23 +25,22 @@
 	// Posts & Posts Meta Tables
 	$postsTable = "CREATE TABLE IF NOT EXISTS posts (
 						post_id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT,
-						post_category_id INT(4) UNSIGNED NOT NULL,
-						post_title VARCHAR(100) NOT NULL,
-						post_author VARCHAR(40) NOT NULL,
+						post_title VARCHAR(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+						post_author VARCHAR(40) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
 						post_date DATE NOT NULL,
-						post_image TEXT NOT NULL,
-						post_content TEXT NOT NULL,
-						post_tags VARCHAR(255),
-						post_comment_count INT(11),
-						post_status VARCHAR(255),
+						post_image TEXT CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+						post_content TEXT CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+						post_tags VARCHAR(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
+						post_comment_count INT(11) UNSIGNED NOT NULL DEFAULT '0',
+						post_status VARCHAR(255) NOT NULL DEFAULT 'draft',
 						PRIMARY KEY (post_id)
 				   ) ENGINE = InnoDB;";
 
 	$postsMetaTable = "CREATE TABLE IF NOT EXISTS posts_meta (
 							meta_id INT(8) UNSIGNED NOT NULL AUTO_INCREMENT,
 							post_id INT(8) UNSIGNED NOT NULL,
-							meta_key VARCHAR(255) NOT NULL,
-							meta_value VARCHAR(255) NOT NULL,
+							meta_key VARCHAR(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+							meta_value VARCHAR(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
 							PRIMARY KEY (meta_id)
 					   ) ENGINE = InnoDB;";
 
